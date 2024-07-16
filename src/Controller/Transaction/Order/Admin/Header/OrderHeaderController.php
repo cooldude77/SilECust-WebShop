abe2edd648f9fb87e292f4b57d17d5fafd87dd69
Ashvini <ashvinisaxena77@gmail.com>
2024-07-16 08:06:34 +0000

    Order Display working in website

--- src/Controller/Transaction/Order/Admin/Header/OrderHeaderController.php
+++ src/Controller/Transaction/Order/Admin/Header/OrderHeaderController.php
@@ -19,8 +19,7 @@ class OrderHeaderController extends AbstractController
 {
     #[Route('/order/create', name: 'order_create')]
     public function createOrderHeader(EntityManagerInterface $entityManager,
-        OrderHeaderDTOMapper $orderHeaderMapper,
-        Request $request
+        OrderHeaderDTOMapper $orderHeaderMapper, Request $request
     ): Response {
         $orderHeaderDTO = new OrderHeaderDTO();
 
@@ -55,10 +54,10 @@ class OrderHeaderController extends AbstractController
         return $this->render('transaction/order/order_create.html.twig', ['form' => $form]);
     }
 
-    #[\Symfony\Component\Routing\Attribute\Route('/order/{id}/edit', name: 'price_product_discount_edit')]
+    #[\Symfony\Component\Routing\Attribute\Route('/order/{id}/edit', name: 'order_edit')]
     public function edit(int $id, OrderHeaderDTOMapper $mapper,
-        EntityManagerInterface $entityManager,
-        OrderHeaderRepository $orderHeaderRepository, Request $request
+        EntityManagerInterface $entityManager, OrderHeaderRepository $orderHeaderRepository,
+        Request $request
     ): Response {
         $orderHeaderDTO = new OrderHeaderDTO();
 
@@ -93,18 +92,37 @@ class OrderHeaderController extends AbstractController
 
     }
 
+    #[Route('/order/{id}/display', name: 'order_display')]
+    public function display(OrderHeaderRepository $OrderHeaderRepository, int $id): Response
+    {
+        $OrderHeader = $OrderHeaderRepository->find($id);
+        if (!$OrderHeader) {
+            throw $this->createNotFoundException('No product found for id ' . $id);
+        }
+
+        $displayParams = ['title' => 'Price',
+                          'link_id' => 'id-price',
+                          'editButtonLinkText' => 'Edit',
+                          'fields' => [['label' => 'id',
+                                        'propertyName' => 'id',],
+                          ]];
+
+        return $this->render(
+            'transaction/order/order_display.html.twig',
+            ['entity' => $OrderHeader, 'params' => $displayParams]
+        );
+    }
+
     #[\Symfony\Component\Routing\Attribute\Route('/order/list', name: 'order_list')]
     public function list(OrderHeaderRepository $orderRepository, PaginatorInterface $paginator,
         Request $request
-    ):
-    Response {
+    ): Response {
 
         $listGrid = ['title' => 'Order',
                      'link_id' => 'id-order',
                      'columns' => [['label' => 'Id',
                                     'propertyName' => 'id',
-                                    'action' => 'display',]
-                                   ,],
+                                    'action' => 'display',],],
                      'createButtonConfig' => ['link_id' => ' id-create-order',
                                               'function' => 'order',
                                               'anchorText' => 'Create Order']];
@@ -112,9 +130,8 @@ class OrderHeaderController extends AbstractController
         $query = $orderRepository->getQueryForSelect();
 
         $pagination = $paginator->paginate(
-            $query, /* query NOT result */
-            $request->query->getInt('page', 1), /*page number*/
-            1 /*limit per page*/
+            $query, /* query NOT result */ $request->query->getInt('page', 1),
+            /*page number*/ 1 /*limit per page*/
         );
 
         return $this->render(
