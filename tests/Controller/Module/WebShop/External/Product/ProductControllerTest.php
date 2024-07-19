dd61e1620f892c3b7eda960ddba29c11bc998827
Ashvini <ashvinisaxena77@gmail.com>
2024-07-19 08:57:48 +0000

    Test for Cart controller modified to include the scenario where non-logged-in user clicks on add to the cart Success

--- tests/Controller/Module/WebShop/External/Product/ProductControllerTest.php
+++ tests/Controller/Module/WebShop/External/Product/ProductControllerTest.php
@@ -2,19 +2,67 @@
 
 namespace App\Tests\Controller\Module\WebShop\External\Product;
 
-use App\Factory\ProductFactory;
-use App\Service\Testing\SessionHelper;
+use App\Tests\Fixtures\CartFixture;
+use App\Tests\Fixtures\CurrencyFixture;
 use App\Tests\Fixtures\CustomerFixture;
+use App\Tests\Fixtures\LocationFixture;
+use App\Tests\Fixtures\PriceFixture;
+use App\Tests\Fixtures\ProductFixture;
+use App\Tests\Fixtures\SessionFactoryFixture;
+use App\Tests\Utility\FindByCriteria;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Zenstruck\Browser;
 use Zenstruck\Browser\Test\HasBrowser;
 
 class ProductControllerTest extends WebTestCase
 {
-    use HasBrowser;
-    use SessionHelper,CustomerFixture;
+    use HasBrowser,
+        CurrencyFixture,
+        CustomerFixture,
+        ProductFixture,
+        PriceFixture,
+        LocationFixture,
+        FindByCriteria,
+        CartFixture,
+        SessionFactoryFixture;
 
+    public function testAddToCart()
+    {
+        $this->createCustomerFixtures();
+        $this->createProductFixtures();
+        $this->createLocationFixtures();
+        $this->createCurrencyFixtures($this->country);
+        $this->createPriceFixtures($this->productA, $this->productB, $this->currency);
 
+        $uriAddProductA = "/cart/product/" . $this->productA->getId() . '/add';
+
+        // From the product page, click on add to cart button
+        $this->browser()
+            // todo: don't allow cart when user is not logged in
+            // not logged-in
+            ->interceptRedirects()
+            ->visit($uriAddProductA)
+            ->fillField('cart_add_product_single_form[productId]', $this->productA->getId())
+            ->fillField(
+                'cart_add_product_single_form[quantity]', 1
+            )
+            ->click('Add To Cart')
+            ->assertRedirectedTo('/login')
+            // now login
+            ->use(callback: function (Browser $browser) {
+                // log in User
+                $browser->client()->loginUser($this->userForCustomer->object());
+            })
+            //Test :  add products to cart
+            ->visit($uriAddProductA)
+            ->fillField('cart_add_product_single_form[productId]', $this->productA->getId())
+            ->fillField(
+                'cart_add_product_single_form[quantity]', 1
+            )
+            ->click('Add To Cart')
+            ->assertSuccessful();
+
+    }
 
 
 }
