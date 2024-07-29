20a1355a7951ce1d7de61edfffd0dc6b48087a25
Ashvini <2662881+cooldude77@users.noreply.github.com>
2024-07-29 06:12:33 +0000

    Wrong pinCode properties were being used for ajax in autocomplete. Corrected Changing the author email

--- src/Controller/Module/WebShop/External/Order/OrderConfirmationController.php
+++ src/Controller/Module/WebShop/External/Order/OrderConfirmationController.php
@@ -2,16 +2,52 @@
 
 namespace App\Controller\Module\WebShop\External\Order;
 
+use App\Controller\Component\UI\Panel\Components\PanelContentController;
+use App\Controller\Component\UI\Panel\Components\PanelHeaderController;
+use App\Controller\Component\UI\PanelMainController;
+use App\Controller\Module\WebShop\External\Shop\HeaderController;
 use App\Repository\OrderHeaderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class OrderConfirmationController extends AbstractController
 {
-
-
     #[Route('/order/{id}/success', 'module_web_shop_order_complete_details')]
+    public function view(Request $request,SessionInterface $session): Response
+    {
+
+
+        $session = $request->getSession();
+
+        $session->set(
+            PanelHeaderController::HEADER_CONTROLLER_CLASS_NAME, HeaderController::class
+        );
+        $session->set(
+            PanelHeaderController::HEADER_CONTROLLER_CLASS_METHOD_NAME,
+            'header'
+        );
+        $session->set(
+            PanelContentController::CONTENT_CONTROLLER_CLASS_NAME, self::class
+        );
+        $session->set(
+            PanelContentController::CONTENT_CONTROLLER_CLASS_METHOD_NAME,
+            'order'
+        );
+
+        $session->set(
+            PanelMainController::BASE_TEMPLATE,
+            'module/web_shop/external/order/page/order_page.html.twig'
+        );
+
+
+        return $this->forward(PanelMainController::class . '::main', ['request' => $request]);
+
+    }
+
+
     public function thankYou(int $id, OrderHeaderRepository $orderHeaderRepository): Response
     {
         // todo: check referring route
