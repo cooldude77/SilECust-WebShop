f6e525b22629eb46ce539e1fa1b4745705334ba1
Ashvini <2662881+cooldude77@users.noreply.github.com>
2024-08-16 15:35:11 +0530

    Order item list is getting displayed. Not all fields are being shown

--- src/Controller/Transaction/Order/Admin/Item/OrderItemController.php
+++ src/Controller/Transaction/Order/Admin/Item/OrderItemController.php
@@ -14,16 +14,17 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class OrderItemController extends AbstractController
 {
     #[Route('/order/{id}/item/create', name: 'order_item_create')]
-    public function create(int $id, EntityManagerInterface $entityManager,
-        OrderItemDTOMapper $orderItemMapper,
-        OrderItemPriceBreakupMapper $orderItemPriceBreakupMapper,
-        Request $request
-    ): Response {
+    public function create(int                         $id, EntityManagerInterface $entityManager,
+                           OrderItemDTOMapper          $orderItemMapper,
+                           OrderItemPriceBreakupMapper $orderItemPriceBreakupMapper,
+                           Request                     $request
+    ): Response
+    {
         $orderItemDTO = new OrderItemDTO();
         $orderItemDTO->orderHeaderId = $id;
 
@@ -62,12 +63,13 @@ class OrderItemController extends AbstractController
         );
     }
 
-    #[\Symfony\Component\Routing\Attribute\Route('/order/item/{id}/edit', name: 'order_item_edit')]
-    public function edit(int $id, OrderItemDTOMapper $mapper,
-        EntityManagerInterface $entityManager, OrderItemRepository $orderItemRepository,
-        OrderItemPriceBreakupMapper $orderItemPriceBreakupMapper,
-        Request $request
-    ): Response {
+    #[Route('/order/item/{id}/edit', name: 'order_item_edit')]
+    public function edit(int                         $id, OrderItemDTOMapper $mapper,
+                         EntityManagerInterface      $entityManager, OrderItemRepository $orderItemRepository,
+                         OrderItemPriceBreakupMapper $orderItemPriceBreakupMapper,
+                         Request                     $request
+    ): Response
+    {
 
         $orderItem = $orderItemRepository->find($id);
 
@@ -113,11 +115,11 @@ class OrderItemController extends AbstractController
         }
 
         $displayParams = ['title' => 'Price',
-                          'link_id' => 'id-price',
-                          'editButtonLinkText' => 'Edit',
-                          'fields' => [['label' => 'id',
-                                        'propertyName' => 'id',],
-                          ]];
+            'link_id' => 'id-price',
+            'editButtonLinkText' => 'Edit',
+            'fields' => [['label' => 'id',
+                'propertyName' => 'id',],
+            ]];
 
         return $this->render(
             'transaction/order/item/order_item_display.html.twig',
@@ -125,21 +127,23 @@ class OrderItemController extends AbstractController
         );
     }
 
-    #[\Symfony\Component\Routing\Attribute\Route('/order/{id}/item/list', name: 'order_item_list')]
-    public function list(int $id, OrderItemRepository $orderItemRepository,
-        PaginatorInterface $paginator,
-        Request $request
-    ): Response {
+    #[Route('/order/{id}/item/list', name: 'order_item_list')]
+    public function list(int                $id, OrderItemRepository $orderItemRepository,
+                         PaginatorInterface $paginator,
+                         Request            $request
+    ): Response
+    {
 
         $listGrid = ['title' => 'Order Items',
-                     'link_id' => 'id-order-items',
-                     'columns' => [['label' => 'Id',
-                                    'propertyName' => 'id',
-                                    'action' => 'display',],],
-                     'createButtonConfig' => ['link_id' => ' id-create-order-item',
-                                              'id' => $id,
-                                              'function' => 'order_item',
-                                              'anchorText' => 'Create Order Item']];
+            'link_id' => 'id-order-items',
+            'function'=>'order_item',
+            'columns' => [['label' => 'Id',
+                'propertyName' => 'id',
+                'action' => 'display',],],
+            'createButtonConfig' => ['link_id' => ' id-create-order-item',
+                'id' => $id,
+                'function' => 'order_item',
+                'anchorText' => 'Create Order Item']];
 
         $query = $orderItemRepository->getQueryForSelect();
 
@@ -150,7 +154,7 @@ class OrderItemController extends AbstractController
 
         return $this->render(
             'admin/ui/panel/section/content/list/list_paginated.html.twig',
-            ['pagination' => $pagination, 'listGrid' => $listGrid]
+            ['pagination' => $pagination, 'listGrid' => $listGrid, 'request' => $request]
         );
     }
 }
