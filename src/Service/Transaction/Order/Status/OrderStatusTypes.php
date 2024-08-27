7acaf23d687504b95be0f9ea493a4fb5e5b3bcf1
Ashvini <2662881+cooldude77@users.noreply.github.com>
2024-08-27 13:28:25 +0530

    Order status type constant made public explicitly

--- src/Service/Transaction/Order/Status/OrderStatusTypes.php
+++ src/Service/Transaction/Order/Status/OrderStatusTypes.php
@@ -5,11 +5,11 @@ namespace App\Service\Transaction\Order\Status;
 readonly class OrderStatusTypes
 {
     public const ORDER_CREATED = "ORDER_CREATED";
-    public const ORDER_PAYMENT_COMPLETE ="ORDER_PAYMENT_COMPLETE";
-    public const ORDER_IN_PROCESS ="ORDER_IN_PROCESS";
-    public const ORDER_SHIPPED ="ORDER_SHIPPED";
-    public const ORDER_COMPLETED ="ORDER_COMPLETED";
-    const ORDER_PAYMENT_FAILED = 'ORDER_PAYMENT_FAILED';
+    public const ORDER_PAYMENT_COMPLETE = "ORDER_PAYMENT_COMPLETE";
+    public const ORDER_IN_PROCESS = "ORDER_IN_PROCESS";
+    public const ORDER_SHIPPED = "ORDER_SHIPPED";
+    public const ORDER_COMPLETED = "ORDER_COMPLETED";
+    public const ORDER_PAYMENT_FAILED = 'ORDER_PAYMENT_FAILED';
 
 
 }
