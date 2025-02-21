dc81224b8fa3cef3f6880234fb84c2991cbb323a
Ashvini <2662881+cooldude77@users.noreply.github.com>
2025-02-21 14:41:40 +0530

    Database migrations successfully added

--- src/SilecustWebShopBundle.php
+++ src/SilecustWebShopBundle.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Silecust\WebShop;
-
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -11,13 +10,11 @@ class SilecustWebShopBundle extends AbstractBundle
     const string CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
 
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        echo __DIR__ . '/Resources/config/*';
 
-       // $container->import(__DIR__ . '/Resources/config/*' . self::CONFIG_EXTS, 'glob');
-        $container->import(__DIR__ . '/Resources/config/repository.yaml');
-        
+        // prepend config from a file
+        $container->import(__DIR__ . '/Resources/config/doctrine_migrations.yaml');
     }
 
 
