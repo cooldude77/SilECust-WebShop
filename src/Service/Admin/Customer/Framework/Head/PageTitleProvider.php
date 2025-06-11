3e9b07a0d28962db468dea5fa155ffc528d3a537
Ashvini <2662881+cooldude77@users.noreply.github.com>
2025-06-11 09:10:08 +0530

    Basic titles for my_ pages are working good

--- src/Service/Admin/Customer/Framework/Head/PageTitleProvider.php
+++ src/Service/Admin/Customer/Framework/Head/PageTitleProvider.php
@@ -15,12 +15,18 @@ readonly class PageTitleProvider
     {
         $route = $this->router->match($request->getPathInfo());
 
-        switch ($route['_route']) {
-            case 'sc_my':
-                return 'Welcome to your dashboard';
-
-
-        }
-        return 'No Title';
+        return match ($route['_route']) {
+            'sc_my', 'sc_my_dashboard' => 'Welcome to your dashboard',
+            'sc_my_profile' => 'Your Profile',
+            'sc_my_orders' => 'Your Orders',
+            'sc_my_personal_info' => 'Your Personal Info',
+            'sc_my_addresses' => 'Your Addresses',
+            'sc_my_address_create' => 'Create new address',
+            'sc_my_address_edit' => 'Edit address',
+            'sc_my_address_display' => 'Display Address',
+            'sc_my_order_display' => 'Order Details',
+            'sc_my_order_item_display' => 'Order Item Details',
+            default => 'No Title',
+        };
     }
 }
