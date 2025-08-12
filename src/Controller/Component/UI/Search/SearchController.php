5045cd948ef283aefc2043a0efc00c52809bedbc
Ashvini <2662881+cooldude77@users.noreply.github.com>
2025-08-12 11:25:39 +0530

    Duplicate search term was getting created in the system

--- src/Controller/Component/UI/Search/SearchController.php
+++ src/Controller/Component/UI/Search/SearchController.php
@@ -2,14 +2,24 @@
 
 namespace Silecust\WebShop\Controller\Component\UI\Search;
 
-use Silecust\WebShop\Form\Common\UI\Search\SearchForm;
 use Silecust\Framework\Service\Component\Controller\EnhancedAbstractController;
+use Silecust\WebShop\Form\Common\UI\Search\SearchForm;
+use Silecust\WebShop\Service\Component\UI\Search\SearchTermReplacer;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * Called when search from "form" is invoked
+ */
 class SearchController extends EnhancedAbstractController
 {
+
+    public function __construct(EventDispatcherInterface $eventDispatcher, private SearchTermReplacer $searchTermReplacer)
+    {
+        parent::__construct($eventDispatcher);
+    }
 
     #[Route('/admin/search', name: 'sc_admin_grid_search')]
     public function search(Request $request): Response
@@ -22,9 +32,13 @@ class SearchController extends EnhancedAbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $url = $request->query->get('_redirected_to');
-            return $this->redirect( "$url&searchTerm={$form->getData()['searchTerm']}");
-}
-        return $this->render('@SilecustWebShop/common/ui/search/search_form.html.twig', ['form' => $form, 'request' => $request]);
+            return $this->redirect($this->searchTermReplacer
+                ->checkAndReplaceSearchTerm($url, $form->getData()['searchTerm']));
+        }
+        return $this->render('@SilecustWebShop/common/ui/search/search_form.html.twig',
+            ['form' => $form, 'request' => $request]);
 
     }
+
+
 }
